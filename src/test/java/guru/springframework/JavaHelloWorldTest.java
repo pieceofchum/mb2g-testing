e8010678986ae33722b9e5ed1dc0e7520c574f76
pieceofchum <pieceofchum@yahoo.com>
2020-06-07 22:07:24 -0400

    Adding junit4 test

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -7,7 +7,6 @@ public class JavaHelloWorldTest {
     @org.junit.Test
     public void getHello() {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
-
         assertEquals("Hello World", javaHelloWorld.getHello());
     }
 }
