534deb4f6dae48d3eeb0751f6685d5cfb8c14c7f
pieceofchum <pieceofchum@yahoo.com>
2020-06-08 20:50:38 -0400

    Adding spock and junit5 tests

---
+++ src/test/java/guru/springframework/JavaJunit5HelloWorldTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaJunit5HelloWorldTest {
+
+    @Test
+    void getHello() {
+    }
+}
